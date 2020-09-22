294f2614706dc0babeb0894238c5b3cfc56f8d3e
LearnCodeWithDurgesh <55891143+LearnCodeWithDurgesh@users.noreply.github.com>
2020-09-22 22:22:47 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "qZnM0O90ToVsLfLIjBiq9GvT9382";
+const API_KEY = "qZnM0O90ToIjBiq9GvT9382";
 
 export const getMatches = () => {
   const url = `https://cricapi.com/api/matches?apikey=${API_KEY}`;
